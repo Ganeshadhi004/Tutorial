028fffe0d5e9cbc9498ce930b0deae4de585a719
Ganeshadhi004 <adhiganesh296@gmail.com>
2025-04-06 00:35:50 +0530

    Delete Tutorial 5 directory

--- Tutorial 5/Prog-07.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-data = {
-    'Car': ['BMW', 'Benz', 'Tesla'],
-    'Make': [2000, 2015, 2025],
-    'Price': [19000, 45000, 100]
-}
-df = pd.DataFrame(data)
-print(df)
--- Tutorial 5/Prog-09.py
+++
@@ -1,3 +0,0 @@
-import pandas as pd
-df = pd.read_csv("auto.csv")
-print(df.head(5))
